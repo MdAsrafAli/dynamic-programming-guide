70e67df43c9751ca2ba5226e9528b1f00509e991
MdAsrafAli <107364717+MdAsrafAli@users.noreply.github.com>
2025-09-06 16:15:38 +0600

    Create fib_recursive.cpp

---
+++ solutions/Foundation/fib_recursive.cpp
@@ -0,0 +1,16 @@
+/*Time Complexity: O(2^n)
+Space Complexity: O(n) - recursion stack
+fib_recursive.cpp */
+#include <bits/stdc++.h>
+using namespace std;
+
+long long fib_rec(int n) {
+    if (n <= 1) return n;
+    return fib_rec(n-1) + fib_rec(n-2);
+}
+
+int main() {
+    int n = 5;
+    cout << "fib(" << n << ") = " << fib_rec(n) << "\n";
+    return 0;
+}
